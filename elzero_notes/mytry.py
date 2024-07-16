46ba8042c8fdc783c0758bf06617d38abdebed6c
kyrolles <kyrollesraafat@gmail.com>
2024-07-16 23:55:08 +0300

    hhha

---
+++ elzero_notes/mytry.py
@@ -0,0 +1,477 @@
+# mystring = "    i love python do you    "
+# print(mystring.strip())
+# print(mystring.lstrip())
+# print(mystring.rstrip())
+
+# print("------------------------------------------\n")
+
+
+# print(mystring.title())
+# print(mystring.capitalize())
+
+# print("------------------------------------------\n")
+
+# c,d,e,f = "1", "11", "111" ,"1111"
+
+# ko = c.zfill(4)
+# print(ko.replace("0"," "))
+# print(d.zfill(4))
+# print(e.zfill(4))
+# print(f.zfill(4))
+
+
+# print("------------------------------------------\n")
+
+# g = "Kyrolles"
+# h = "kYROLLES"
+
+# print(g.upper())
+# print(h.lower())
+
+
+# print("------------------------------------------")
+# print("------------------------------------------\n")
+
+
+# a = "I-Love-Python-and-PHP-and-MySQL"
+# print(a.split("-",3))
+
+
+# p = "I-Love-Python-and-PHP-and-MySQL"
+# print(p.rsplit("-",3))
+
+
+# print("------------------------------------------\n")
+
+# i = "kyrolles"
+# print(i.center(12))
+# print(i.center(12,"@"))
+
+# s = "I hate coffe i really really heate coffe"
+
+# print(s.center(25,"*") )
+
+
+# print("------------------------------------------\n")
+
+# print(s.count("coffe",0,25))
+
+# print("------------------------------------------\n")
+
+# print(s.swapcase())
+
+# print("------------------------------------------\n")
+
+# # startswith()
+
+# i = "I hate coffe"
+# print(i.startswith("I"))
+# print(i.startswith("S"))
+# print(i.startswith("c", 7))
+
+# print("------------------------------------------\n")
+
+# print(i.endswith("e"))
+# print(i.endswith("e",2))
+# print(i.endswith("o",7))
+
+# print("------------------------------------------\n")
+ 
+# a = "i love python"
+# print(a.find("r"))
+
+# b = "i really really love coffe but today i hate it"
+# print(b.rjust(100))
+# print(b.ljust(100,"_"))
+
+# print("------------------------------------------\n")
+
+# e = """First Line
+# Second Line
+# Third Line"""
+
+# print(e.splitlines())
+
+# print("------------------------------------------\n")
+
+# # expandtabs()
+
+# g = "Hello\tWorld\tI\tLove\tPython"
+# print(g.expandtabs(2))
+
+# print("------------------------------------------\n")
+
+# a = "i was very very very ........... today because i spill my coffe"
+# print(a.replace("very","ha",2))
+
+# mylist= ["kyrolles","raafat","lamey"]
+
+# print("-".join(mylist))
+
+# print("------------------------------------------\n")
+
+# x = "hello i hate coffe"
+# y = 188837
+# z = 484738.3874
+# print(f"the str is: {x:.13s}\n and the num is: {y:d}\n and the flooat is {z:.2f}\n")
+
+# print(len(x))
+
+# n = 3+2j
+# print(type(n))
+# print("\n")
+# print(f"the num is {n}")
+# print(f"the num is {n.real}")
+# print(f"the num is {n.imag}")
+
+# print("------------------------------------------\n")
+
+# print(120//20)
+# print(120//19)
+
+
+# print("------------------------------------------\n")
+
+# myAwesomeList =["one","two",1,True,100.5]
+
+# print(myAwesomeList)
+# print(myAwesomeList[2:4])
+# print(myAwesomeList[-1])
+# print(myAwesomeList[::2])
+# myAwesomeList[1:3] = "a"
+# print(myAwesomeList)
+
+# print("------------------------------------------\n")
+# print(type(myAwesomeList[-1]))
+# print(type(myAwesomeList[-2]))
+# print(type(myAwesomeList[-3]))
+# print(type(myAwesomeList[-4]))
+# print("------------------------------------------\n")
+
+# mylist=["kok","noon","Amzon","coffe","Black"]
+# oldfriend=["mina","abanoub","claudio"]
+# mylist.append("ho_oh")
+# mylist.append(oldfriend)
+# print(mylist)
+# print(mylist[-1])
+# print(type(mylist[-1]))
+# mylist=["kok","noon","amzon","coffe","black"]
+# oldfriend=["mina","abanoub","claudio","kok"]
+# mylist.extend(oldfriend)
+# print(mylist)
+# mylist.remove("kok")
+# print(mylist)
+
+# print("------------------------------------------\n")
+
+# # mylist.sort()
+# mylist.reverse()
+# print(mylist)
+
+# print("------------------------------------------\n")
+
+# a =[1,2,3,4]
+# a.clear()
+# print(a)
+
+# d =[1,2,34,5,5,55]
+# #copy
+# c = d.copy()
+# print(c)
+
+# d.append(6)
+# print(d)
+# print(c)
+
+# print(d.count(5))
+# print("------------------------------------------\n")
+# print(d.index(5))
+
+# d.insert(5,"ho_ho")
+# print(d)
+
+# d.insert(-1,"ho_ho")
+# print(d)
+# print(d.pop(-1))
+# print(d)
+
+
+# # -- Tuple --
+# print("------------------------------------------\n")
+
+# myAwesomeTupleOne =("kiro","also me")
+# myAwesomeTupleTwo ="kiro","also me"
+# print(myAwesomeTupleOne)
+# print(myAwesomeTupleTwo)
+
+# print(type(myAwesomeTupleOne))
+# print(type(myAwesomeTupleTwo))
+
+# myAwesomeTupleThree =("ho",1,2,4,"ho_ho",45,63,699,5.5)
+# myAwesomeListThree =["ho",1,2,4,45,"ho_ho",63,699,5.5]
+# print(myAwesomeTupleThree)
+# print(myAwesomeTupleThree[0])
+
+# print(myAwesomeTupleThree[-1])
+# print(myAwesomeTupleThree[-2])
+
+# print(type(myAwesomeTupleThree[-2]))
+# print(type(myAwesomeListThree[-2]))
+
+# print("------------------------------------------\n")
+
+# myAwesomelistr = ["one","two","three",100,10.5,True]
+# print(type(myAwesomelistr[-1]))
+# print(type(myAwesomelistr[-2]))
+# print(type(myAwesomelistr[-3]))
+
+
+# print("------------------------------------------\n")
+
+# # myAwesomeTupleFour = (1, 2, 3, 4, 5)
+# # myAwesomeTupleFour[2] = 5 
+# # print(myAwesomeTupleFour)  # 'tuple' object does not support item assignment
+
+# print("------------------------------------------\n")
+
+# myAwesomeTupleFive = ("Osama", "Osama", 1, 2, 3, 100.5, True)
+# print(myAwesomeTupleFive[1])
+
+# print("------------------------------------------\n")
+
+
+# a = [1, 2, 3, 4]
+# b = [5, 6]
+
+
+
+
+
+
+
+
+
+
+
+# myString = "Osama"
+# myList = [1, 2]
+# myTuple = ("A", "B")
+
+# print(myString * 6)
+# print(myList * 6)
+# print(myTuple * 6)
+
+# print("------------------------------------------\n")
+
+# b = (1, 3, 7, 8, 2, 6, 5)
+# # print("The Position of Index Is: " + b.index(7))  # Error
+# print("The Position of Index Is: {:d}".format(b.index(7)))
+# print(f"The Position of Index Is: {b.index(7)}")
+
+
+# a = ("ho","ne","tt",4)
+
+# x,y,_,z = a
+# print(x," ",y," ",z)
+
+# print("------------------------------------------\n")
+
+# a =0
+# while a < 10:
+#     print(f"Hello Number {a}")
+#     a += 1
+# else:
+#     print(f"hello again last {a}")
+
+# print("------------------------------------------\n")
+
+# myf =["fd","mi","mr","be","cl","os","pt","ys"]
+# a=0
+# while a < len(myf):
+#     print(f"hello friend num #{str(a+1).zfill(2)} name is {myf[a]}")
+#     a +=1
+
+# print("------------------------------------------\n")
+
+# myfavouritwep = []
+# limt = 5
+# while 5 > len(myfavouritwep):
+#     web = input("wesite name without https://")
+#     myfavouritwep.append(f"https://{web.strip().lower()}")
+#     limt-=1
+#     print(f"website added, {limt} places left")
+
+# myfavouritwep.sort()
+
+# if myfavouritwep:
+#     o = 0
+#     while o < len(myfavouritwep):
+#         print(myfavouritwep[o])
+#         o+=1
+
+# print("------------------------------------------\n")
+
+# tries = 5
+# mainpass = "kero"
+# inpass = input("Enther the password: ")
+
+# while mainpass != inpass:
+
+#     tries -= 1
+#     print(f"worng password {'last' if tries == 0 else tries} chance left")
+
+#     inpass = input("Enther the password: ")
+
+#     if tries == 0:
+#         print("All Tries Is Finished.")
+#         break
+
+# else:
+#     print("correct pass")
+
+# print("------------------------------------------\n")
+
+# myNumber = [1,2,3,4,5,6,7,8,9,10]
+
+# for num in myNumber:
+#     if num % 2 == 0:
+#         print(f"The Number {num} is Even")
+#     else:
+#         print(f"The Number {num} is Odd")
+
+# for it in "kyrolles":
+#     print(f"the letter: {it.upper()}")
+
+# print("------------------------------------------\n")
+
+# myrange = range(1,100)
+# print(myrange)
+
+# for num in myrange:
+#     print(f"hello i'am {num}")
+
+# myskills ={
+#     "html":"8%",
+#     "css":"2%",
+#     "pyhton":"10%",
+#     "c":"20%",
+# }
+# for skill in myskills:
+#     print(f"my skill is {skill} & the progress is : {myskills[skill]}")
+
+# print("------------------------------------------\n")
+
+# peoples = {
+#   "Osama": {
+#     "Html": "70%",
+#     "Css": "80%",
+#     "Js": "70%"
+#   },
+#   "Ahmed": {
+#     "Html": "90%",
+#     "Css": "80%",
+#     "Js": "90%"
+#   },
+#   "Sayed": {
+#     "Html": "70%",
+#     "Css": "60%",
+#     "Js": "90%"
+#   }
+# }
+
+# for name in peoples:
+#     print(f"{name} : ")
+#     for skill in peoples[name]:
+#         print(f"{skill} => {peoples[name][skill]}")
+            
+# print("------------------------------------------\n")
+
+# mynumber = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+
+# for num in mynumber:
+#     if num == 13 or  num == 3:
+#         continue
+#     print(num)
+# print("------------------------------------------\n")
+# for num in mynumber:
+#     print(num)
+#     if num == 13 :
+#           break
+#     print(num)
+# print("------------------------------------------\n")
+# for num in mynumber:
+#     if num == 13 or  num == 3:
+#         pass
+#     print(num)
+
+# print("------------------------------------------\n")
+
+# myUltimateSkills = {
+#   "HTML": {
+#     "Main": "80%",
+#     "Pugjs": "80%"
+#   },
+#   "CSS": {
+#     "Main": "90%",
+#     "Sass": "70%"
+#   }
+# }
+
+# for main_skill,main_value in myUltimateSkills.items():
+#     print(f"{main_skill}:")
+#     for child_skill,child_value in main_value.items():
+#         print(f"{child_skill} => {child_value}")
+
+# def first_function_in_python():
+#     return "Hello my real name kok"
+
+
+# print(first_function_in_python())
+
+# a,b,c = "kok","Re","YOs"
+
+# def say_hello(name):
+#     print(f"Hello, {name}")
+
+# say_hello("kyrolles")
+# say_hello(a)
+# say_hello(b)
+# say_hello(c)
+
+# def Add(x,y):
+#     if type(x) != int or type(y) != int:
+#         print("invalid input type it should be integar")
+#         return(-1)
+#     else:
+#         return(x+y)
+
+# print(Add("ho",200))
+
+# def full_name(first,middle,last):
+#     print(f"{first.strip().capitalize()} {middle.upper():.1s} {last.capitalize()}")
+
+# full_name("    kyrolleS    ","rAAFAT","lamE")
+
+# print("------------------------------------------\n")
+
+# def say_hello(*the_n):
+#     for name in the_n:
+#         print(f"Hello, {name.title()}")
+# say_hello("youssef","mohamed","kyrolles","steven","youyou","anna")
+
+# def show_details(name,*skills):
+#     print(f"hello,{name.title()} your skills are:")
+#     for skill in skills:
+#         print(f"{skill.title()}")
+
+# show_details("youanna","css","js","python","english")
+
+# print("------------------------------------------\n")
+
+# def say_hello(name,age,country = "unknown"):
+#     print(f"Hello,{name.title()} Your Age is {age} and Your Country Is {country.title()}")
+
+# say_hello("kyrolles",23,"Egypt")
+# say_hello("youssef",21)
+
+# print("------------------------------------------\n")
