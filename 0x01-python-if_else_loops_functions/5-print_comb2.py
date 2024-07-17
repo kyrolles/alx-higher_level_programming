a4a914de6035aebe3822b7dd7f178a1a0fab99d3
kyrolles <kyrollesraafat@gmail.com>
2024-07-17 07:51:29 +0300

    5-print_comb2.py edit 4

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
     if num == 99:
-        print("{} \n".format(str(num).zfill(2)), end='')
+        print("{}\n".format(str(num).zfill(2)), end='')
     else:
         print("{}, ".format(str(num).zfill(2)), end='')
