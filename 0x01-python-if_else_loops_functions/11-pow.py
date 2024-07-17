af9bf9169a3f11c9adebde219eb10481bf0b4403
kyrolles <kyrollesraafat@gmail.com>
2024-07-17 14:41:34 +0300

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def pow(a, b):
-    x = 1
-    while b != 0:
-        x *= a
-        b -= 1
-    return (x)
+    # x = 1
+    # while b != 0:
+    #     x *= a
+    #     b -= 1
+    # return (x)
+    pow(a**b)
+print(pow(2,2))
