e2cc2e9befcbfb628aff0b67d158f011cd98a97b
kyrolles <kyrollesraafat@gmail.com>
2024-07-17 11:49:26 +0300

    6-print_comb3.py edit 1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,6 +6,6 @@ for num1 in range(0, 9):
         elif num1 > num2:
             continue
         elif num1 == 8 and num2 == 9:
-            print("{}\n".format((str(num1)+str(num2)).zfill(2)))
+            print("{}".format((str(num1)+str(num2)).zfill(2)))
         else:
             print("{}, ".format((str(num1)+str(num2)).zfill(2)), end='')
