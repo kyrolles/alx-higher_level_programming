81e991bcfdc5e93a3f75a4a6cf2306203660ce4d
kyrolles <kyrollesraafat@gmail.com>
2024-07-17 15:29:25 +0300

    100-print_alpha_reverse edit 1

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 alpha = 122
-while 122 >= alpha >= 96:
-    print("{}".format(chr(alpha)), end='')
+while 122 >= alpha > 96:
+    print("{}".format(chr(alpha)if alpha % 2 == 0 else chr(alpha - 32)), end='\
+')
     alpha -= 1
