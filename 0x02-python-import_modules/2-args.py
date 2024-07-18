39c2628528a57a06f850587d2e0f994e0c822de2
kyrolles <kyrollesraafat@gmail.com>
2024-07-18 15:35:27 +0300

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+if len(argv) == 1:
+    print("{} argument: ".format(len(argv)-1))
+elif len(argv) == 0:
+    print("{} arguments.".format(len(argv)-1))
+else:
+    print("{} arguments: ".format(len(argv)-1))
+
+i = 1
+arguments = argv[1:]
+for num in arguments:
+    print("{}: {}".format(i, num))
+    i += 1
