b6ff05fcdec94277fd1d864453329805f4e69082
kyrolles <kyrollesraafat@gmail.com>
2024-07-28 18:17:55 +0300

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    lenght = len(sentence) 
-    first=sentence[0]
-    return lenght,first
+    lenght = len(sentence)
+    first = sentence[0]
+    return lenght, first
