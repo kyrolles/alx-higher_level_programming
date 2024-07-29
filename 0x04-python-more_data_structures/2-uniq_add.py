b672592bcac2ee2afb6dad516010e59c499d4784
kyrolles <kyrollesraafat@gmail.com>
2024-07-29 18:45:46 +0300

    2uniq-add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from functools import reduce
 def uniq_add(my_list=[]):
     my_set = set(my_list)
-    result =reduce(lambda a,b:a+b,my_set) 
-    return result
+    x = 0
+    for i in my_set:
+        x += i
+    return x
