85d196cc7a4e02d50d972b2e31aaa6616f19a48a
kyrolles <kyrollesraafat@gmail.com>
2024-07-30 17:43:58 +0300

    12-roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
-import roman
-
-
 def roman_to_int(roman_string):
     if roman_string is not str(roman_string) or roman_string is None:
         return 0
-    return (roman.fromRoman(roman_string))
+    Rom_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+    result = 0
+    for i in range(len(roman_string)):
+        if i > 0 and Rom_map[roman_string[i]] > Rom_map[roman_string[i-1]]:
+            result += Rom_map[roman_string[i]] - 2*Rom_map[roman_string[i-1]]
+        else:
+            result += Rom_map[roman_string[i]]
+
+    return result
