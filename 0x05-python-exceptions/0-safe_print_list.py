a2a934bc76bd9708cf5f51ba3d4b6dbb3681e546
kyrolles <kyrollesraafat@gmail.com>
2024-07-31 13:51:05 +0300

    0-safe_print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+def safe_print_list(my_list=[], x=0):
+    try:
+        i = 0
+        for num, list in enumerate(my_list):
+            if num < x:
+                print(f"{list}", end='')
+            else:
+                break
+            i += 1
+        print()
+        return (i)
+    except:
+        print("Erro")
