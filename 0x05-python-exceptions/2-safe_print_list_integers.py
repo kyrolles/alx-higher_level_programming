d33e4237b3222e1aa35f91859e48b8bea182e62a
kyrolles <kyrollesraafat@gmail.com>
2024-08-04 14:26:11 +0300

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        indx = 0
+        printed = 0
+        while printed < x:
+            if type(my_list[indx]) == int:
+                print("{:d}".format(my_list[indx]), end='')
+                printed += 1
+            else:
+                x -= 1
+            indx += 1
+        print()
+        return printed
+    except ValueError:
+        return False
